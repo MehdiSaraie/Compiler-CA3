5e6bdc22580cd3702fc691cf13adb7bb98df253d
Slifer-The-Sky-Dragon <aghaei.alirezza@gmail.com>
2021-06-06 00:08:43 +0430

    Adding Files

---
+++ TypeSetter.java
@@ -0,0 +1,103 @@
+package main.visitor;
+
+import main.ast.nodes.*;
+import main.ast.nodes.declaration.*;
+import main.ast.nodes.expression.*;
+import main.ast.nodes.expression.values.*;
+import main.ast.nodes.expression.values.primitive.*;
+import main.ast.nodes.statement.*;
+import main.ast.types.*;
+import main.ast.types.functionPointer.FptrType;
+import main.ast.types.single.BoolType;
+import main.compileErrors.typeErrors.ConditionNotBool;
+import main.compileErrors.typeErrors.ReturnValueNotMatchFunctionReturnType;
+import main.compileErrors.typeErrors.UnsupportedTypeForPrint;
+import main.symbolTable.*;
+import main.symbolTable.exceptions.*;
+import main.symbolTable.items.*;
+import main.symbolTable.utils.Stack;
+
+import java.util.*;
+
+public class TypeSetter  extends Visitor<Void> {
+    public static main.symbolTable.utils.Stack<FunctionSymbolTableItem> func_stack = new Stack<>();
+    public static ArrayList<String> visited_function_name = new ArrayList<>();
+    public static ArrayList<FunctionDeclaration> visited_function_declaration = new ArrayList<>();
+
+    private TypeInference typeInference = new TypeInference();
+    @Override
+    public Void visit(Program program) {
+        program.getMain().accept(this);
+        return null;
+    }
+
+    @Override
+    public Void visit(FunctionDeclaration funcDeclaration) {
+        try {
+            FunctionSymbolTableItem functionSymbolTableItem = (FunctionSymbolTableItem) (SymbolTable.root.getItem(FunctionSymbolTableItem.START_KEY + funcDeclaration.getFunctionName()));
+            TypeSetter.func_stack.push(functionSymbolTableItem);
+            funcDeclaration.getBody().accept(this);
+            TypeSetter.func_stack.pop();
+        }catch (ItemNotFoundException e){}
+
+        return null;
+    }
+
+    @Override
+    public Void visit(MainDeclaration mainDeclaration) {
+        mainDeclaration.getBody().accept(this);
+        return null;
+    }
+
+    @Override
+    public Void visit(BlockStmt blockStmt) {
+        for (Statement stat : blockStmt.getStatements())
+            stat.accept(this);
+        return null;
+    }
+
+    @Override
+    public Void visit(ConditionalStmt conditionalStmt) {
+        Type cond_type = conditionalStmt.getCondition().accept(typeInference);
+        if (!(cond_type instanceof BoolType || cond_type instanceof NoType)){
+            //if error
+        }
+        conditionalStmt.getThenBody().accept(this);
+        Statement else_body = conditionalStmt.getElseBody();
+        if (else_body != null)
+            else_body.accept(this);
+        return null;
+    }
+
+    @Override
+    public Void visit(FunctionCallStmt funcCallStmt) {
+        funcCallStmt.getFunctionCall().accept(typeInference);
+        return null;
+    }
+
+    @Override
+    public Void visit(PrintStmt print) {
+        Type arg_type = print.getArg().accept(typeInference);
+        if (arg_type instanceof FptrType || arg_type instanceof VoidType){
+            //print error
+        }
+        return null;
+    }
+
+    @Override
+    public Void visit(ReturnStmt returnStmt) {
+        Type cur_return_type = returnStmt.getReturnedExpr().accept(typeInference);
+
+        FunctionSymbolTableItem cur_func = TypeSetter.func_stack.pop();
+        Type function_return_type = cur_func.getReturnType();
+
+        if(function_return_type == null || function_return_type instanceof NoType){
+            cur_func.setReturnType(cur_return_type);
+        }
+        // TODO error return type
+
+        TypeSetter.func_stack.push(cur_func);
+
+        return null;
+    }
+}
